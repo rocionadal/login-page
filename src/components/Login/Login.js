7e54a2b51ef602ae668080958fa0d6e49a046288
rocionadal <rocioonadal@gmail.com>
2022-02-20 13:30:29 +0100

    using useEffect cleanup function

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,10 +12,18 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
-  }, [enteredEmail, enteredPassword])
+    const identifier = setTimeout(() => {
+      console.log('Checking form');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      clearTimeout(identifier);
+      console.log('Clearing');
+    }
+   }, [enteredEmail, enteredPassword])
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
