0ad35997e083abbebe5f04dd632873f7e00baee0
rocionadal <rocioonadal@gmail.com>
2022-02-26 14:59:54 +0100

    refactor to cleaner code

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
@@ -31,8 +32,17 @@ const Login = (props) => {
   // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-  const [emailState, dispatchEmail] = useReducer(emailReducer, { value: '', isValid: null });
-  const [passwordState, dispatchPassword] = useReducer(passwordReducer, { value: '', isValid: null });
+  const [emailState, dispatchEmail] = useReducer(emailReducer, { 
+    value: '', 
+    isValid: null 
+  });
+
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, { 
+    value: '', 
+    isValid: null 
+  });
+
+  const ctx = useContext(AuthContext);
 
   const { isValid: emailIsValid } = emailState;
   const { isValid: passwordIsValid } = passwordState;
@@ -75,7 +85,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    ctx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
